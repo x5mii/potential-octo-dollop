11f9944823235bd716c50b4f8abac179fbd7dd25
sprudello <110892985+sprudello@users.noreply.github.com>
2024-08-29 12:00:40 +0200

    Added Company Class

---
+++ potential-octo-dollop/Functions/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace potential_octo_dollop.Functions
+{
+    class Company
+    {
+        public double PaidInCapital { get; set; }
+        public double GeneralReserve { get; set; }
+        public double AnnualProfit { get; set; }
+
+        public Company() 
+        {
+            
+        }
+    }
+}

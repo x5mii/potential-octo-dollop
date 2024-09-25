233d72434c4a16e91b965dbb2b37b5fb778e7d96
Samuel Lucena <110893302+x5mii@users.noreply.github.com>
2024-09-25 18:35:42 +0200

    Revert "Update Calculation.cs"

--- potential-octo-dollop/Functions/Calculation.cs
+++ potential-octo-dollop/Functions/Calculation.cs
@@ -88,7 +88,7 @@ namespace potential_octo_dollop.Functions
             }
             else
             {
-                return availableProfit;
+                return availableProfit; // Return the maximum possible dividend
             }
         }
 
